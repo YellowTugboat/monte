a6a4e4fc892a8b2c4fa7bc8aebcd1565d4ebd612
Kevin Buhrer <kevin@yellowtugboat.com>
2016-12-18 08:32:48 -0700

    Update tools export barrel

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,7 +1,9 @@
 export { isArray, isDefined, isFunc, isNumeric, isObject, isString } from './is';
+export { commonEventNames } from './commonEventNames';
 export { compose } from './compose';
 export { getDepthFirst } from './getDepthFirst';
 export { mergeOptions, ReplacePreceding } from './mergeOptions';
 export { noop } from './noop';
 export { readTransforms } from './transform';
 export { resetScaleDomain } from './resetScaleDomain';
+export { camelCase, kebabCase, snakeCase, pascalCase, upperFirst } from './string';
