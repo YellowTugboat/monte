ae72ef6b62758bf01f9708d40efb9f4e3e3f2073
Kevin Buhrer <kevin@yellowtugboat.com>
2016-12-18 08:31:24 -0700

    Add custom message support for error types

--- src/support/MonteError.js
+++ src/support/MonteError.js
@@ -2,10 +2,11 @@
 // http://stackoverflow.com/questions/31089801/extending-error-in-javascript-with-es6-syntax
 
 export class MonteError extends Error {
-  constructor(message) {
-    super(message);
+  constructor(message, ...msgs) {
+    const msg = [message, ...msgs].join(' ');
+    super(msg);
     this.name = this.constructor.name;
-    this.message = message;
+    this.message = msg;
 
     if (typeof Error.captureStackTrace === 'function') {
       Error.captureStackTrace(this, this.constructor);
@@ -15,11 +16,11 @@ export class MonteError extends Error {
     }
   }
 
-  static UnimplementedMethod(method, methodName, sourceType='chart') {
-    return new MonteError(`${method} (\`${methodName}\`) needs to be defined in order for the ${sourceType} to be useful.`);
+  static UnimplementedMethod(method, methodName, sourceType='chart', ...msgs) {
+    return new MonteError(`${method} (\`${methodName}\`) needs to be defined in order for the ${sourceType} to be useful.`, ...msgs);
   }
 
-  static InvalidArgumentType(methodName, argumentName, expectedType, received) {
-    return new MonteError(`Method (${methodName}) argument "${argumentName}" of unexpected type. Expected ${expectedType}, but was given ${received}.`);
+  static InvalidArgumentType(methodName, argumentName, expectedType, received, ...msgs) {
+    return new MonteError(`Method (${methodName}) argument "${argumentName}" of unexpected type. Expected ${expectedType}, but was given ${received}.`, ...msgs);
   }
 }
--- src/support/MonteOptionError.js
+++ src/support/MonteOptionError.js
@@ -1,10 +1,10 @@
 import { MonteError } from './MonteError';
 export class MonteOptionError extends MonteError {
-  static RequiredOption(optionName) {
-    return new MonteError(`Option "${optionName}" is required.`);
+  static RequiredOption(optionName, ...msgs) {
+    return new MonteError(`Option "${optionName}" is required.`, ...msgs);
   }
 
-  static InvalidEnumOption(optionName, badValue) {
-    return new MonteError(`Option "${optionName}" must be set to a valid option. The value "${badValue}" is not valid.`);
+  static InvalidEnumOption(optionName, badValue, ...msgs) {
+    return new MonteError(`Option "${optionName}" must be set to a valid option. The value "${badValue}" is not valid.`, ...msgs);
   }
 }
